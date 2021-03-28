80ccfbca9b1886565c945ec6604cd808a542e14f
iruzts <ruizsoto1996@gmail.com>
2021-03-28 07:48:00 -0600

    migraciones

---
+++ database/migrations/2021_03_28_133417_create__usuario_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_usuario', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->integer('telefono');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_usuario');
+    }
+}
---
+++ database/migrations/2021_03_28_133947_create__cliente_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClienteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_cliente', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->integer('telefono');
+            $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_cliente');
+    }
+}
